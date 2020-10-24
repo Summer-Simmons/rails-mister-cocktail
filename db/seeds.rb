ff2f25eed26bf7957a491ab19957d0fe49f3a5a6
Summer S <summer.s2020@icloud.com>
2020-10-24 17:52:59 +0900

    controllers done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning cocktail database..."
+Cocktail.destroy_all
 
+puts "Creating cocktails..."
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Beer")
+Cocktail.create(name: "Pina Colada")
+puts "Finished!"
+
+puts "Cleaning ingredients database..."
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+Ingredient.create(name: "mint leaves")
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+puts "Finished!"
+
+puts "Cleaning doses database..."
+Dose.destroy_all
+
+puts "Creating doses..."
+Dose.create(description: "put mint leaves", ingredient: "1", cocktail_id: "1" )
+Dose.create(description: 'Put mint leaves', ingredient: Ingredient.first, cocktail: CockTail.first)
